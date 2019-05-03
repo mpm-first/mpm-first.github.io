b2dcd16e973e7b62302d87f57dc8bdf2a6ec3b7b
Christopher McClellan <chris@Christophers-MacBook-Air.local>
2019-05-03 20:50:25 +0800

    finished ascyrus content

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,39 +1,43 @@
 jQuery(function($) {
 
-    var $win = $(window);
-    var $doc = $(document);
-    var $html = $('html');
-    var $body = $('body');
+  var hash = window.location.hash;
+  if (hash.trim()) window.location = window.location.href.replace(hash, "");
 
-    //
-    // Toggles the footer to display as
-    // a fixed overlay menu
-    //
-    $('.burger').click(function(event) {
-      event.preventDefault();
-      if ($body.hasClass('opened'))
-        return $body.removeClass('opened');
-      $body.addClass('opened');
-    });
+  var $win = $(window);
+  var $doc = $(document);
+  var $html = $('html');
+  var $body = $('body');
 
-    //
-    // Hides the burger if we're scrolled near the footer
-    // since the footer is our main navigation
-    //
-    function burger(event) {
-      var scrolled   = $win.scrollTop() + $win.height();
-      var til_footer = $doc.height() - $win.height();
-      var has_class  = $body.hasClass('bottom');
-      if (scrolled >= til_footer && !has_class)
-        return $body.addClass('bottom');
-      else if (scrolled < til_footer && has_class)
-        return $body.removeClass('bottom');
-    }
-    $win.on('scroll', burger);
+  //
+  // Toggles the footer to display as
+  // a fixed overlay menu
+  //
+  $('.burger').click(function(event) {
+    event.preventDefault();
+    if ($body.hasClass('opened'))
+      return $body.removeClass('opened');
+    $body.addClass('opened');
+  });
 
-    //
-    // Video player
-    //
+  //
+  // Hides the burger if we're scrolled near the footer
+  // since the footer is our main navigation
+  //
+  function burger(event) {
+    var scrolled   = $win.scrollTop() + $win.height();
+    var til_footer = $doc.height() - $win.height();
+    var has_class  = $body.hasClass('bottom');
+    if (scrolled >= til_footer && !has_class)
+      return $body.addClass('bottom');
+    else if (scrolled < til_footer && has_class)
+      return $body.removeClass('bottom');
+  }
+  $win.on('scroll', burger);
+
+  //
+  // Video player
+  //
+  if (!!$('#player').length) {
     var player_config = {
       controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
       debug: true
@@ -44,6 +48,7 @@ jQuery(function($) {
     $video_anchor.on('click.home', function(event) {
       $('#video').show();
       player.play();
-    })
+    });
+  }
 
 });
